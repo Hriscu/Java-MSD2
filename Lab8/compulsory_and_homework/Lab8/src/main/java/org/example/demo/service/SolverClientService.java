caaf2f2133bc00799f7cc36a64d37063453b735e
Hriscu Alexandru-Gabriel <79144041+Hriscu@users.noreply.github.com>
2025-12-14 12:00:29 +0200

    push solution Lab8

--- Lab8/compulsory_and_homework/Lab8/src/main/java/org/example/demo/service/SolverClientService.java
+++ Lab8/compulsory_and_homework/Lab8/src/main/java/org/example/demo/service/SolverClientService.java
@@ -1,22 +1,31 @@
 package org.example.demo.service;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class SolverClientService {
 
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate;
     private final String SOLVER_URL = "http://localhost:8083/api/solver/solve";
+
+    public SolverClientService() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+
+        factory.setConnectTimeout(1000);
+        factory.setReadTimeout(1000);
+
+        this.restTemplate = new RestTemplate(factory);
+    }
 
     @Retry(name = "solverRetry", fallbackMethod = "solveFallback")
     public String invokeStableMatch(Object payload) {
-        System.out.println("--- Calling StableMatch Service ---");
+        System.out.println("--- Calling StableMatch Service (Timeout set to 1s) ---");
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -27,7 +36,7 @@ public class SolverClientService {
     }
 
     public String solveFallback(Object payload, Throwable t) {
-        System.out.println("!!! StableMatch is DOWN or Error. Executing Fallback logic. Error: " + t.getMessage());
+        System.out.println("!!! StableMatch Failed (Timeout or Down). Executing Fallback. Error: " + t.getMessage());
         return "{ \"assignments\": [], \"unassignedStudents\": [\"ALL_STUDENTS_FALLBACK\"] }";
     }
 }
